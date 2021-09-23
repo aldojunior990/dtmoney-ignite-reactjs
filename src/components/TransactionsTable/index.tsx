4bfa4282b367350181deb3d5462e214579df80d2
aldojunior990 <aldojunior990@gmail.com>
2021-09-23 15:16:28 -0300

    valores e datas formatados ao padrão brasileiro

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -40,9 +40,22 @@ export function TransactionsTable(){
 
                         <tr key={transaction.id}>
                             <td>{transaction.title}</td>
-                            <td className={transaction.type}>{transaction.amount}</td>
-                            <td>{transaction.category}</td>
-                            <td>{transaction.createdAt}</td>
+
+                                 <td className={transaction.type}>{new Intl.NumberFormat('pt-br', {
+                                    style: 'currency',
+                                    currency: 'BRL'
+                                    }).format(transaction.amount)}
+                                </td>
+
+                                <td>{transaction.category}</td>
+                                <td>{
+                                    
+                                    new Intl.DateTimeFormat('pt-br').format(
+                                        new Date(transaction.createdAt)
+                                    )
+                                    }
+                                    
+                                </td>
                         </tr>
 
                         )   
