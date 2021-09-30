5bc8aa172e8bc7533abc37c3ab700a0afa4da185
aldojunior990 <aldojunior990@gmail.com>
2021-09-30 00:31:23 -0300

    correções

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,77 +1,71 @@
-
-import incomeImg from '../../assets/income.svg'
-import outcomeImg from '../../assets/outcome.svg'
-import totalImg from '../../assets/total.svg'
-import { useTransactions } from '../../hooks/useTransactions';
-
-
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import totalImg from "../../assets/total.svg";
+import { useTransactions } from "../../hooks/useTransactions";
 
 import { Container } from "./styles";
 
-export function Summary(){
+export function Summary() {
+  const { transactions } = useTransactions();
 
-    const {transactions} = useTransactions()
-    
-    const summary = transactions.reduce((acc, transaction) => {
-        if (transaction.type == 'deposit'){
-            acc.deposits += transaction.amount;
-            acc.total += transaction.amount;
-        }else{
-            acc.withdraws += transaction.amount;
-            acc.total -= transaction.amount;
-        }
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "deposit") {
+        acc.deposits += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.withdraws += transaction.amount;
+        acc.total -= transaction.amount;
+      }
 
-        return acc;
+      return acc;
+    },
+    {
+      deposits: 0,
+      withdraws: 0,
+      total: 0,
+    }
+  );
 
-    },{
-        deposits: 0,
-        withdraws: 0,
-        total: 0,
-    })
-    
-    return(
-       <Container>
-           <div>
-               <header>
-                   <p>Entradas</p>
-                   <img src={incomeImg} alt="Entradas" />
-               </header>
-               <strong>
-                    {new Intl.NumberFormat('pt-br', {
-                     style: 'currency',
-                     currency: 'BRL'
-                     }).format(summary.deposits)
-                    }
-                </strong>
-           </div>
-           <div>
-               <header>
-                   <p>Saidas</p>
-                   <img src={outcomeImg} alt="Saidas" />
-               </header>
-               <strong>-
-                   
-                   {new Intl.NumberFormat('pt-br', {
-                     style: 'currency',
-                     currency: 'BRL'
-                     }).format(summary.withdraws)
-                    }</strong>
-
-           </div>
-           <div className="back">
-               <header>
-                   <p>Total</p>
-                   <img src={totalImg} alt="Total" />
-               </header>
-               <strong> 
-                   
-                   {new Intl.NumberFormat('pt-br', {
-                     style: 'currency',
-                     currency: 'BRL'
-                     }).format(summary.total)
-
-                    }</strong>
-           </div>
-       </Container>
-    )
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="Entradas" />
+        </header>
+        <strong>
+          {new Intl.NumberFormat("pt-br", {
+            style: "currency",
+            currency: "BRL",
+          }).format(summary.deposits)}
+        </strong>
+      </div>
+      <div>
+        <header>
+          <p>Saidas</p>
+          <img src={outcomeImg} alt="Saidas" />
+        </header>
+        <strong>
+          -
+          {new Intl.NumberFormat("pt-br", {
+            style: "currency",
+            currency: "BRL",
+          }).format(summary.withdraws)}
+        </strong>
+      </div>
+      <div className="back">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Total" />
+        </header>
+        <strong>
+          {new Intl.NumberFormat("pt-br", {
+            style: "currency",
+            currency: "BRL",
+          }).format(summary.total)}
+        </strong>
+      </div>
+    </Container>
+  );
 }
