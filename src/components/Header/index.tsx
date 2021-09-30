00dfb6ba20ad54f4bd19c886860a651333e8ea92
aldojunior990 <aldojunior990@gmail.com>
2021-09-30 00:27:09 -0300

    correções

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,9 +3,6 @@ import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
 
-interface HeaderProps {
-    onOpenNewTransactionModal: () => void
-}
 
 export function Header(){
     return(
--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,77 +1,63 @@
-import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { api } from "../services/api";
 
-
-interface Transaction{
-    id: number
-    title: string
-    amount: number
-    type: string
-    category: string
-    createdAt: string
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
 }
 
-type TransactionInput = Omit<Transaction, 'id'| 'createdAt'>
+type TransactionInput = Omit<Transaction, "id" | "createdAt">;
 
-
-interface TransactionsProviderProps{
-    children: ReactNode
+interface TransactionsProviderProps {
+  children: ReactNode;
 }
 
-
-
-interface TransactionsContextData{
-    transactions: Transaction[]
-    createTransaction: (transaction: TransactionInput) => Promise<void>
+interface TransactionsContextData {
+  transactions: Transaction[];
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
-interface LocalTransaction {
+export const TransactionsContext = createContext<TransactionsContextData>(
+  {} as TransactionsContextData
+);
 
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    api
+      .get("transactions")
+      .then((response) => setTransactions(response.data.transactions));
+  }, []);
+
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post("/transactions", {
+      ...transactionInput,
+      createdAt: new Date(),
+    });
+
+    const { transaction } = response.data;
+
+    setTransactions([...transactions, transaction]);
+  }
+  return (
+    <TransactionsContext.Provider value={{ transactions, createTransaction }}>
+      {children}
+    </TransactionsContext.Provider>
+  );
 }
-export const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData)
 
-
-
-export function TransactionsProvider({children}: TransactionsProviderProps){
-    
-    const [transactions, setTransactions] = useState<Transaction[]>([])
-
-    useEffect(()=> {
-        api.get('transactions')
-        .then(response => setTransactions(response.data.transactions))
-    }, [])
-
-    async function createTransaction(transactionInput: TransactionInput){
-       const response =  await api.post('/transactions', {
-           ...transactionInput,
-           createdAt: new Date(),
-       })
-
-       const { transaction } = response.data;
-
-       setTransactions([...transactions, transaction ])
-
-       function localTransactions(localTransac: LocalTransaction){
-
-    
-
-       }
-   
-          
-      }
-        return (
-
-        <TransactionsContext.Provider value = {{ transactions, createTransaction }}>
-            {children}
-        </TransactionsContext.Provider>
-    )
-} 
-
-
-
-export function useTransactions(){
-
-    const context = useContext(TransactionsContext)
-    return context
-
+export function useTransactions() {
+  const context = useContext(TransactionsContext);
+  return context;
 }
